fb78bc727b642d7800e393a269596cc7e96f049f
Magomed Akhmaev <m.axmaev@yandex.ru>
2024-02-29 01:39:15 +0300

    Added random color generation with button

---
+++ app.js
@@ -0,0 +1,50 @@
+const cols = document.querySelectorAll('.col')
+
+
+document.addEventListener('keydown', (e)=>{
+    e.preventDefault();
+    if(e.code.toLowerCase()==='space'){
+        setRandomColors();
+    }
+})
+
+document.addEventListener('click', (e)=>{
+   const type = e.target.dataset.type;
+   if (type === 'lock') {
+    const node = e.target.tagName.toLowerCase()==='i' ? e.target : e.target.children[0]
+
+    node.classList.toggle('fa-lock-open');
+    node.classList.toggle('fa-lock');
+   }
+})
+
+
+function generateRandomColor(){
+    const hexCodes = '0123456789ABCDEF'
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    return '#' + color
+}
+
+function setRandomColors(){
+    cols.forEach(col =>{
+        const text = col.querySelector('h2')
+        const color = chroma.random()
+        
+        text.textContent = color;
+        col.style.background = color;
+
+        setTextColor(text, color);
+    })
+}
+
+function setTextColor(text, color){
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+
+setRandomColors()
+
